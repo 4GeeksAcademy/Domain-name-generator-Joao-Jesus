b3bca9a445dae83142d24a18a6a0394b4c578dff
joaoaugusto-jesus <147534481+joaoaugusto-jesus@users.noreply.github.com>
2024-02-16 19:39:34 +0000

    Domain-Generator-review

--- src/app.js
+++ src/app.js
@@ -7,5 +7,30 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domain = [".eu", ".pt"];
+
+  let domainGenerator = [];
+
+  for (let index in pronoun) {
+    domainGenerator[0] = pronoun[index];
+    for (let index1 in adj) {
+      domainGenerator[1] = adj[index1];
+      for (let index2 in noun) {
+        domainGenerator[2] = noun[index2];
+        for (let index3 in domain) {
+          domainGenerator[3] = domain[index3];
+          console.log(
+            domainGenerator[0] +
+              domainGenerator[1] +
+              domainGenerator[2] +
+              domainGenerator[3]
+          );
+        }
+      }
+    }
+  }
 };
